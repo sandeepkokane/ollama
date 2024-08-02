4c801d98cebc39181ba32e1947dd4dc9395241e7
sandeep.kokane <sandeepkokane97@gmail.com>
2024-08-02 11:02:23 +0530

    - initial commit

---
+++ src/main/java/com/boot/Ollama3Application.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ollama3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ollama3Application.class, args);
+    }
+
+}
